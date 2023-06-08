64b4a90a586a421f92b7ef4948714af2a2446dbb
dkessner laptop <Darren.Kessner@marlborough.org>
2023-06-08 08:45:13 -0700

    fixes for linux

--- hello_nextflow/nextflow_varcal/session1/run_nfcore_viralint_test.sh
+++ hello_nextflow/nextflow_varcal/session1/run_nfcore_viralint_test.sh
@@ -1,1 +1,2 @@
 nextflow run nf-core/viralintegration -r 0.1.0 -profile test,docker --outdir test_results
+#nextflow run nf-core/viralintegration -r 0.1.0 -profile test,singularity --outdir test_results
