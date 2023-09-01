4c7f0bf483b1f614cfc7e1e252299492f7238936
Darren Kessner <dkessner@eitm.org>
2023-09-01 10:01:58 -0700

    play with R / xcms

--- R/xcms_install.R
+++ R/xcms_install.R
@@ -10,4 +10,24 @@ BiocManager::install("xcms")
 # in R interactive mode, run html server
 # browseVignettes("xcms")
 
+# BiocStyle required for xcms vignette examples to run (!)
+
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("BiocStyle")
+
+# and faahKO (wtf?!)
+
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("faahKO")
+
+
+# and pander and pheatmap (!)
+
+install.packages("pander")
+install.packages("pheatmap")
+
 
