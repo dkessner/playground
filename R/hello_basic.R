1c50aaca87c0cace78ab47947ed18040d9f7bd13
Darren Kessner <dkessner@eitm.org>
2023-10-11 14:43:25 -0700

    new file:   hello_basic.R

---
+++ R/hello_basic.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+# command-return to send selection (or line) to console
+
+# get/set working directory
+getwd()
+setwd("blah")
+
+log10(100)
+log10(1000)
+exp(1)
+
+log(1000, 10)
+
+# boolean values
+1 == 0 
+1 == 1 
+
+
+x <- 1/40
+x
