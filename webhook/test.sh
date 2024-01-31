b2c08cb3af1022df38cec47656bb430e7acee847
Darren Kessner <dkessner@eitm.org>
2024-01-30 16:58:54 -0800

    added test.sh

---
+++ webhook/test.sh
@@ -0,0 +1,4 @@
+curl -X "POST" -H "Content-Type: application/json" -d '{"webserviceUrl":"http://0.0.0.0:8090/"}' http://0.0.0.0:8090/hello_world
+#curl -X "POST" -H "Content-Type: application/json" -d '{"webserviceUrl":"http://0.0.0.0:8090/"}' http://0.0.0.0:8090/new_goo
+#curl -X "POST" -H "Content-Type: application/json" -d '{"webserviceUrl":"http://0.0.0.0:8090/"}' http://0.0.0.0:8090/feedback_form
+#curl -X "POST" -H "Content-Type: application/json" -d '{"webserviceUrl":"http://0.0.0.0:8090/"}' http://0.0.0.0:8090/eln/check_number_samples
