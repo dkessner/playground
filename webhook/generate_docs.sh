ac724037e06050b6b41ceb771ded9aaf692dcb1e
Darren Kessner <dkessner@eitm.org>
2024-02-02 10:44:06 -0800

    generate_docs: generate html, at package level

--- webhook/generate_docs.sh
+++ webhook/generate_docs.sh
@@ -3,16 +3,23 @@
 # generate_docs.sh
 #
 
+mkdir -p docs
+cd docs
 
-for class in sapiopylib.rest.WebhookService.AbstractWebhookHandler \
-sapiopylib.rest.DataMgmtService.DataMgmtServer \
-sapiopylib.rest.pojo.webhook.WebhookContext.SapioWebhookContext \
-sapiopylib.rest.pojo.webhook.WebhookResult.SapioWebhookResult \
-sapiopylib.rest.WebhookService.WebhookConfiguration \
-sapiopylib.rest.WebhookService.WebhookServerFactory
+#for class in sapiopylib.rest.WebhookService.AbstractWebhookHandler \
+#sapiopylib.rest.DataMgmtService.DataMgmtServer \
+#sapiopylib.rest.pojo.webhook.WebhookContext.SapioWebhookContext \
+#sapiopylib.rest.pojo.webhook.WebhookResult.SapioWebhookResult \
+#sapiopylib.rest.WebhookService.WebhookConfiguration \
+#sapiopylib.rest.WebhookService.WebhookServerFactory
+
+
+for class in sapiopylib.rest.WebhookService \
+sapiopylib.rest.DataMgmtService \
+sapiopylib.rest.pojo.webhook.WebhookContext 
 do
     echo $class
-    python -m pydoc $class
+    python -m pydoc -w $class 
 done
 
 
