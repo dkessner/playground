d14a01c6c5bcfb449d895fc16c52312f5c418e71
Darren Kessner <dkessner@eitm.org>
2024-09-19 16:09:04 -0700

    added classification head

--- transformers/nlpbook/nlpbook_ch3_transformer.py
+++ transformers/nlpbook/nlpbook_ch3_transformer.py
@@ -201,3 +201,26 @@ encoded = encoder(inputs.input_ids)
 print("encoded.size():", encoded.size())
 
 
+## classification head
+
+class TransformerForSequenceClassification(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.encoder = TransformerEncoder(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+    def forward(self, x):
+        x = self.encoder(x)[:, 0, :]
+        x = self.dropout(x)
+        x = self.classifier(x)
+        return x
+        
+       
+config.num_labels = 3
+encoder_classifier = TransformerForSequenceClassification(config)
+encoded = encoder_classifier(inputs.input_ids)
+print("encoded.size():", encoded.size())
+
+
+
