1a82dde0c557683239b0c33f25a3568389d20f84
Darren Kessner <dkessner@eitm.org>
2024-09-19 14:42:13 -0700

    modified:   nlpbook_ch3_transformer.py
    saving work, feed forward

--- transformers/nlpbook/nlpbook_ch3_transformer.py
+++ transformers/nlpbook/nlpbook_ch3_transformer.py
@@ -112,10 +112,25 @@ multihead_attn = MultiHeadAttention(config)
 attn_output = multihead_attn.forward(inputs_embeds)
 print("attn_output.size:", attn_output.size())
 
+## feed-forward layer
 
+class FeedForward(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.linear_1 = nn.Linear(config.hidden_size, config.intermediate_size)
+        self.linear_2 = nn.Linear(config.intermediate_size, config.hidden_size)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
+    def forward(self, x):
+        x = self.linear_1(x)
+        x = self.gelu(x)
+        x = self.linear_2(x)
+        x = self.dropout(x)
+        return x
 
-
-
+feed_forward = FeedForward(config)
+ff_outputs = feed_forward(attn_outputs)
+print("ff_outputs.size():", ff_outputs.size())
 
 
