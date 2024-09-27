bb4dfcf19be4d5c9385242ecf91a6500857e3b00
Darren Kessner <dkessner@eitm.org>
2024-09-26 17:13:27 -0700

    added comments / q&a

--- transformers/training_bert.py
+++ transformers/training_bert.py
@@ -113,6 +113,57 @@ for epoch in range(epochs):
         loop.set_postfix(loss=loss.item())
 
 
+# question:
+#
+# how does optim.step() update the parameters?  isn't the gradient stored in
+# loss?
+#
+# https://pytorch.org/docs/stable/optim.html
+#
+
+
+# answer from stackoverflow
+
+# Rather than thinking about how loss and parameters are related, you should
+# consider them as separate events which are not linked. Indeed, there are two
+# distinct elements that have an effect on parameters and their cached
+# gradient.
+
+# The autograd mechanism (the process in charge of performing gradient
+# computation) allows you to call backward on a torch.Tensor (your loss) and
+# which will in turn backpropagate through all the nodes tensors that are
+# allowed to compute this final tensor value. Doing so, it will navigate
+# through what's called the computation graph, updating each of the parameters'
+# gradients by changing their grad attribute. This means that at the end of a
+# backward call the network's learned parameters that were used to compute this
+# output will have a grad attribute containing the gradient of the loss with
+# respect to that parameter.
+
+# loss.backward()
+
+# The optimizer is independent of the backward pass since it doesn't rely on
+# it. You can call backward on your graph once, multiple times, or on different
+# loss terms depending on your use case. The optimizer's task is to take the
+# parameters of the model independently (that is irrespective of the network
+# architecture or its computation graph) and update them using a given
+# optimization routine (for example via Stochastic Gradient Descent, Root Mean
+# Squared Propagation, etc...). It goes through all parameters it was
+# initialized with and updates them using their respective gradient value
+# (which is supposed to be stored in the grad attribute by at least one
+# backpropagation.
+
+# optimizer.step()
+
+# Important notes:
+
+# Keep in mind though that the backward process and the actual update call
+# using the optimizer are linked implicitly only by the fact that the optimizer
+# will use the results computed by the backward preceding call.
+
+# In PyTorch parameter gradients are kept in memory so you have to clear them
+# out before performing a new backward call. This is done using the optimizer's
+# zero_grad function. In practice, it clears the grad attribute of the tensors
+# it has registered as parameters.
 
 
 
